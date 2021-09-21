a6b088b60f02ce4f579e9027d5b779cac6b4e821
Zahra Yousef <45607455+zoozi515@users.noreply.github.com>
2021-09-21 10:09:51 +0300

    Days of the Week - Version #1

---
+++ src/WeekDays.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val day = mapOf(1 to "Sunday", 2 to "Monday", 3 to "Tuesday", 4 to "Wednesday",
+        5 to "Thursday", 6 to "Fiday", 7 to "Saturday")
+
+    for (i in 1..7)
+        println(day[i])
+
+}
